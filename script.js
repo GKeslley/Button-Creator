1c1971ae79bb2583691708c98dae8dabf0da16da
Guilherme Keslley <guilhermekeslley123@gmail.com>
2022-07-11 18:04:20 -0300

    applying LocalStorage

--- script.js
+++ script.js
@@ -40,8 +40,24 @@ function formTarget(event) {
   const value = event.target.value;
   const name = event.target.name;
   buttonStyle[name](value);
+  saveValues(name, value);
   cssTextHtml();
 }
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach((propertie) => {
+    buttonStyle[propertie](localStorage[propertie]);
+    form.elements[propertie].value = localStorage[propertie];
+  });
+  cssTextHtml();
+}
+
+setValues();
 
 function cssTextHtml() {
   cssText.innerHTML =
